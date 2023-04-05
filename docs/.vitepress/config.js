32a4ba8cc2d66c701340dca8fc4f3a4a3b22e804
何嘉泳 <893404614@qq.com>
2023-04-05 20:00:32 +0800

    feat：新增工具库

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -4,27 +4,47 @@ import { defineConfig } from 'vitepress'
 export default defineConfig({
   title: "学海无涯",
   description: "A VitePress Site",
-  base:'/Knowledge-is-infinite/', 
+  base: '/Knowledge-is-infinite/',
   themeConfig: {
     // https://vitepress.dev/reference/default-theme-config
-    logo:"https://s1.ax1x.com/2023/04/03/pph0ZWQ.jpg",
+    logo: "https://s1.ax1x.com/2023/04/03/pph0ZWQ.jpg",
 
     // 导航栏
     nav: [
       { text: '首页', link: '/' },
-      { text: '前端技巧', link: '/skills/markdown-examples' }
+      { text: '前端技巧', link: '/skills/markdown-examples' },
+      { text: '文档', link: '/documents/markdown-examples' },
+      { text: '工具库', link: '/tools/sprites' }
     ],
 
     // 侧边栏
-    sidebar: [
-      {
-        text: '前端技巧 ',
-        items: [
-          { text: 'Markdown Examples', link: '/skills/markdown-examples' },
-          { text: 'Runtime API Examples', link: '/skills/api-examples' }
-        ]
-      }
-    ],
+    sidebar: {
+      '/skills/': [
+        {
+          text: '前端技巧',
+          items: [
+            { text: 'Markdown Examples', link: '/skills/markdown-examples' },
+            { text: 'Runtime API Examples', link: '/skills/api-examples' }
+          ]
+        },
+      ],
+      '/documents/': [
+        {
+          text: '文档',
+          items: [
+            { text: '文档1', link: '/skills/markdown-examples' },
+          ]
+        },
+      ],
+      '/tools/': [
+        {
+          text: '工具库',
+          items: [
+            { text: '精灵图转换', link: '/tools/sprites' },
+          ]
+        },
+      ]
+    },
 
     // 社交链接
     socialLinks: [
---
+++ docs/tools/sprites.md
@@ -0,0 +1,7 @@
+
+<script setup>
+import Sprites from './components/Sprites.vue'
+</script>
+
+<Sprites/>
+
