c8bd7b6d958de5a79086809a9e7c886964e82be8
JY-HE <JY-HE@users.noreply.github.com>
2023-05-14 02:42:12 +0000

    deploy: 1741d89fadd7ca0ce0b69d7114b87aa098131b5b

--- assets/skills_textGradient.md.16da2393.lean.js
+++
@@ -1,1 +0,0 @@
-import{_ as s,o as a,c as n,N as l}from"./chunks/framework.e35b7731.js";const A=JSON.parse('{"title":"文字颜色渐变多种效果","description":"","frontmatter":{},"headers":[],"relativePath":"skills/textGradient.md"}'),p={name:"skills/textGradient.md"},o=l("",13),e=[o];function t(c,r,D,y,F,i){return a(),n("div",null,e)}const d=s(p,[["render",t]]);export{A as __pageData,d as default};
